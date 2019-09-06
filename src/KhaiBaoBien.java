8772e12a2fa5d91a5b6d01d85ad7b17070469290
Ac Quy Khong Ten <nhuconghaui@gmail.com>
2019-09-06 14:18:00 +0700

    upfile

---
+++ src/KhaiBaoBien.java
@@ -0,0 +1,19 @@
+public class KhaiBaoBien {
+    public static void main(String[] args) {
+        int i=10;
+        float f = 10.5f;
+        double a=10.25;
+        boolean b=true;
+        char c='A';
+        String abc="nguyen nhu cong";
+        System.out.println("int"+i);
+        System.out.println("float"+f);
+        System.out.println("double"+a);
+        System.out.println("boolean"+b);
+        System.out.println("char"+i);
+        System.out.println("string"+i);
+
+
+
+    }
+}
